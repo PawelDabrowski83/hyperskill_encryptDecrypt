8404bda3cd83c38a7fc51cfe9aa495dda380c6af
Paweł Dąbrowski <48104564+PawelDabrowski83@users.noreply.github.com>
2020-06-05 14:21:03 +0200

    TDD - unit tests for stage1, now ready to write a method

--- src/test/java/org/hyperskill/encryptDecrypt/MainTest.java
+++ src/test/java/org/hyperskill/encryptDecrypt/MainTest.java
@@ -1,4 +1,100 @@
 package org.hyperskill.encryptDecrypt;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class MainTest {
+
+    @Test
+    public void shouldFindOppositeLetterGivenAReturnsZ() {
+        // given
+        char c = 'a';
+        char expected = 'z';
+
+        // when
+        char actual = Main.findOppositeLetter(c);
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindOppositeLetterGivenCReturnsX() {
+        // given
+        char c = 'c';
+        char expected = 'x';
+
+        // when
+        char actual = Main.findOppositeLetter(c);
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindOppositeLetterGivenUpperBReturnsUpperY() {
+        // given
+        char c = 'B';
+        char expected = 'Y';
+
+        // when
+        char actual = Main.findOppositeLetter(c);
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindOppositeLetterGivenHReturnsS() {
+        // given
+        char c = 'h';
+        char expected = 's';
+
+        // when
+        char actual = Main.findOppositeLetter(c);
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindOppositeLetterGivenUpperMReturnsUpperN() {
+        // given
+        char c = 'M';
+        char expected = 'N';
+
+        // when
+        char actual = Main.findOppositeLetter(c);
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindOppositeLetterGivenHashReturnsHash() {
+        // given
+        char c = '#';
+        char expected = '#';
+
+        // when
+        char actual = Main.findOppositeLetter(c);
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindOppositeLetterGivenExclamationReturnsExclamation() {
+        // given
+        char c = '#';
+        char expected = '#';
+
+        // when
+        char actual = Main.findOppositeLetter(c);
+
+        // then
+        assertEquals(expected, actual);
+    }
+
 }
