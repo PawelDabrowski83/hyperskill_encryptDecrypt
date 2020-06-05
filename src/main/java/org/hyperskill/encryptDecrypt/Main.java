7cf5017e48886c0dab81d263fec6d9e5301f21d2
Paweł Dąbrowski <48104564+PawelDabrowski83@users.noreply.github.com>
2020-06-05 14:36:13 +0200

    Stage 1 after tests

--- src/main/java/org/hyperskill/encryptDecrypt/Main.java
+++ src/main/java/org/hyperskill/encryptDecrypt/Main.java
@@ -2,7 +2,21 @@ package org.hyperskill.encryptDecrypt;
 
 public class Main {
 
+    protected static final int LOWERCASE_A = (int) 'a';
+    protected static final int LOWERCASE_Z = (int) 'z';
+    protected static final int UPPERCASE_A = (int) 'A';
+    protected static final int UPPERCASE_Z = (int) 'Z';
+
     public static char findOppositeLetter(char c) {
+        int charNumber = (int) c;
+        if (charNumber >= LOWERCASE_A && charNumber <= LOWERCASE_Z) {
+            int rangeToZ = LOWERCASE_Z - charNumber;
+            return (char) (LOWERCASE_A + rangeToZ);
+        }
+        if (charNumber >= UPPERCASE_A && charNumber <= UPPERCASE_Z) {
+            int rangeToZ = UPPERCASE_Z - charNumber;
+            return (char) (UPPERCASE_A + rangeToZ);
+        }
         return c;
     }
 
